f4e44d8b19749baeb068501ce3d4b46b625d8ccb
RenjieChiang <1037253670@qq.com>
2021-04-01 16:37:59 +0800

    " commit----20210401pm17"

--- main.cpp
+++ main.cpp
@@ -26,17 +26,17 @@ int main()
     TreeNode * root;
     auto myTree = new TreeNode[5] ;
     myTree[0].val = 5;
-//    myTree[0]->left = myTree[1];
+    myTree[0].left = &myTree[1];
     myTree[0].right = &myTree[2];
     myTree[1].val = 1;
     myTree[2].val = 4;
-    myTree[2].left = &myTree[1];
-    myTree[2].right = &myTree[2];
-
+    myTree[2].left = &myTree[3];
+    myTree[2].right = &myTree[4];
     myTree[3].val = 3;
-
     myTree[4].val = 6;
-
     TreeOut(myTree);
+    std::cout << std::endl;
+    std::cout << mySolution.isValidBST(myTree);
+    delete [] myTree ;
     return 0;
 }
