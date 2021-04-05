cea048210825c3f5fa4ed0354b9ec64ef894acd8
RenjieChiang <1037253670@qq.com>
2021-04-05 20:45:01 +0800

    " commit----20210405pm21"

---
+++ LCPrimarySort_Search.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tianbot on 2021/4/5.
+//
+
+#include "LCPrimarySort_Search.h"
+#include "algorithm"
+void LCPrimarySort_Search::merge(std::vector<int> &nums1, int m, std::vector<int> &nums2, int n)
+{
+//    std::copy(nums2.begin(), nums2.end(), nums1.begin()+m);
+//    std::sort(nums1.begin(),nums1.end());
+
+}
---
+++ LCPrimarySort_Search.h
@@ -0,0 +1,16 @@
+//
+// Created by tianbot on 2021/4/5.
+//
+
+#ifndef MYLEETCODE_LCPRIMARYSORT_SEARCH_H
+#define MYLEETCODE_LCPRIMARYSORT_SEARCH_H
+
+#include "vector"
+class LCPrimarySort_Search {
+public:
+    void merge(std::vector<int> &nums1, int m, std::vector<int>& nums2, int n);
+
+};
+
+
+#endif //MYLEETCODE_LCPRIMARYSORT_SEARCH_H
